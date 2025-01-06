d0dcb4ba2d114f1410d3f20a82d443adc4845a91
cd ~/code/$deyvisong/dotfiles && zsh git_setup.sh <deyvisonalves@protonmail.com>
2025-01-06 21:17:56 +0100

    NavLink

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,20 @@
 // 2 - links with react router
 import "./Navbar.css";
 
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+      {/*<Link to="/">Home</Link>
+      <Link to="/about">About</Link>*/}
+      <NavLink
+        to="/"
+        // className={(isActive) => (isActive ? "is-active" : "not-active")}
+      >
+        Home
+      </NavLink>
+      <NavLink to="/about">About</NavLink>
     </nav>
   );
 };
